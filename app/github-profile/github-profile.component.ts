fd311ed351766050c82d5499209db3d990463454
shubhangi251 <mishra25anju10@gmail.com>
2020-03-14 12:02:37 +0530

    2 files modified

--- app/github-profile/github-profile.component.ts
+++ app/github-profile/github-profile.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute,Router} from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
   templateUrl: './github-profile.component.html',
   styleUrls: ['./github-profile.component.css']
 })
-export class GithubProfileComponent implements OnInit {
+export class GithubProfileComponent {
 
-  constructor(private router:Router) { }
-submit(){
-  this.router.navigate(['/followers'],
-  {
-    queryParams:{page:1,order:'new'}
-  })
-}
+  constructor(private router: Router) { }
+  submit() {
+    this.router.navigate(['/followers'],
+      {
+        queryParams: { page: 1, order: 'new' }
+      });
+  }
 
 }
